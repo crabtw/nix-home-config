fd0557b8fd55b3e75a408ad29a9bdb0b2a05477b
Jyun-Yan You <jyyou.tw@gmail.com>
2020-04-18 09:00:48 +0800

    Remove tig workaround

--- overlays/local.nix
+++ overlays/local.nix
@@ -31,10 +31,4 @@ in
   );
 
   vmware-horizon-client = super.callPackage ../pkgs/vmware-horizon-client.nix {};
-
-  tig = super.tig.overrideAttrs (old: {
-    installPhase = old.installPhase + ''
-      ln -s ${self.git}/share/bash-completion/completions/git $out/etc/bash_completion.d/
-    '';
-  });
 }
